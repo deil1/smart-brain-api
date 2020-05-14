7b8d4c18f9d417b2b7d9ef736c56e4f150264313
deil1 <diltamang1@gmail.com>
2020-05-14 19:36:09 +0530

    database update 1

--- server.js
+++ server.js
@@ -12,8 +12,11 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-   	connectionString : process.env.DATABASE_URL,
-    ssl: true,
+   	host : 'postgresql-objective-51478',
+    user: 'deil';
+    password: '45191991',
+    database: 'smart-brain'
+
   }
 });
 
